7442b7bdbb621efc4209b3dce92e340ca00f8f5c
ferrophile <hwpang@ust.hk>
2016-04-24 02:46:44 +0800

    Get data via Bash

--- cgi-bin/test.sh
+++ cgi-bin/test.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
 echo "Hello!"
+
+file='91M_1.csv'
+regex='"response":\[(.*)\]'
+
+while IFS=',' read -a line; do
+	url="http://etav2.kmb.hk/?action=geteta&lang=en&route=91M&bound=1&stop=${line[2]}&stop_seq=${line[0]}"
+	text="`wget -qO- $url`"
+	if [[ $text =~ $regex ]]
+	then
+		name="${BASH_REMATCH[1]}"
+		echo $name;
+	fi	
+done < $file
