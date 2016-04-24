02ddaf03963a314ab9e86819216f01365ca61820
ferrophile <hwpang@ust.hk>
2016-04-25 07:19:44 +0800

    Produce correct pos; gen map

--- cgi-bin/test.sh
+++ cgi-bin/test.sh
@@ -3,22 +3,38 @@ echo "Hello!"
 
 file='91M_1.csv'
 regex='"response":\[(.*)\]'
-regex2='"ex":".* ([0-9]*):([0-9]*):([0-9]*)"'
-len=0
+regex2='"updated":([0-9]*)'
+regex3='"ex":".* ([0-9]*):([0-9]*):([0-9]*)"'
 
 declare -A data
+declare -A lengths
+declare -A names
+declare -A updated
+declare -A interval
+declare -A dist
 
-
+len=0
 while IFS=',' read -a line; do
 	url="http://etav2.kmb.hk/?action=geteta&lang=en&route=91M&bound=1&stop=${line[2]}&stop_seq=${line[0]}"
 	text="`wget -qO- $url`"
+	
+	names[$len]=${line[1]}
+	interval[$len]=${line[3]}
+	dist[$len]=${line[4]}
+	
+	if [[ $text =~ $regex2 ]]; then
+		stamp="${BASH_REMATCH[1]}"
+		updated[$len]=$(($stamp / 1000 + 28800))
+		updated[$len]=$((${updated[$len]} % 86400))
+	fi
+	
 	if [[ $text =~ $regex ]]
 	then
 		entry="${BASH_REMATCH[1]}"
 		IFS='}' read -a record <<< "$entry"
-		data[${line[0]}, 0, 0]=${#record[@]}
+		lengths[$len]=${#record[@]}
 		for (( i=0; i<=(${#record[@]}-1); i++ )); do
-			if [[ ${record[$i]} =~ $regex2 ]]; then
+			if [[ ${record[$i]} =~ $regex3 ]]; then
 				hour=${BASH_REMATCH[1]#0}
 				min=${BASH_REMATCH[2]#0}
 				sec=${BASH_REMATCH[3]#0}
@@ -28,18 +44,28 @@ while IFS=',' read -a line; do
 					min=`expr $min + 50`
 					hour=`expr $hour - 1`				
 				fi
-				data[${line[0]}, (($i+1)), 0]=$hour
-				data[${line[0]}, (($i+1)), 1]=$min
-				data[${line[0]}, (($i+1)), 2]=$sec
+				if (( i==0 )); then
+					echo "${line[1]}, $hour, $min, $sec"
+				fi
+				data[${line[0]}, $i]=`expr $hour \* 3600 + $min \* 60 + $sec`
 			fi
 		done
 	fi
 	len=`expr $len + 1`
 done < $file
 
-for (( i=0; i<$len; i++ )); do
-	for (( j=0; j<${data[$i, 0, 0]}; j++ )); do
-		printf "${data[$i, (($j+1)), 0]}h, ${data[$i, (($j+1)), 1]}m, ${data[$i, (($j+1)), 2]}s; "
-	done
-	printf "\n"
+for (( i=1; i<$len; i++ )); do
+	j=`expr $i - 1`
+	if (( ${data[$i, 0]} < ${data[$j, 0]} )); then
+		printf "\n${names[$i]}: "
+		if (( ${data[$i, 0]} > ${updated[$i]} )); then
+			remain=$((${data[$i, 0]} - ${updated[$i]}))
+		else
+			remain=0
+		fi
+		offset=$((${dist[$i]} - ${dist[$j]}))
+		offset=$(($offset * $remain / ${interval[$i]}))
+		offset=$((${dist[$i]} - $offset))
+		printf "$remain sec, $offset\n"
+	fi
 done
